361613d2f1c5d873679eca57e5d5f7cccbdad3cd
bluewow <dogseen@gmail.com>
2021-08-31 23:03:25 +0900

    safeArea 추가 / toast message 추가 (no context)

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -90,129 +91,152 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(15.0),
-            color: Color(0xFFF7F1E8),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 12.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    'Find your industry\'s top vetted marketing experts',
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15.0),
+              color: Color(0xFFF7F1E8),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 12.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Find your industry\'s top vetted marketing experts',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Image(
-                  image: AssetImage('docker-logo.png'),
-                  height: 200.0,
-                )
-              ],
+                  Image(
+                    image: AssetImage('docker-logo.png'),
+                    height: 200.0,
+                  )
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 12.0,
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(28.0, 16.0, 28.0, 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Stefan Garlson',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              '4.8',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 12.0,
-                            ),
-                            Icon(Icons.star,
-                                size: 20, color: Colors.deepOrangeAccent[400]),
-                            Icon(Icons.star,
-                                size: 20, color: Colors.deepOrangeAccent[400]),
-                            Icon(Icons.star,
-                                size: 20, color: Colors.deepOrangeAccent[400]),
-                            Icon(Icons.star,
-                                size: 20, color: Colors.deepOrangeAccent[400]),
-                            Icon(Icons.star_half,
-                                size: 20, color: Colors.deepOrangeAccent[400]),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Transform.scale(
-                      scale: 0.8,
-                      child: Builder(
-                        builder: (ctx) {
-                          return FloatingActionButton(
-                            elevation: 0.0,
-                            child: Icon(Icons.add),
-                            onPressed: () {
-                              ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
-                                content: Text('hello'),
-                              ));
-                              print('clicked');
-                            },
-                          );
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Table(
-              border: TableBorder.all(
-                width: 1.0,
-                style: BorderStyle.solid,
-                color: Colors.grey.shade200,
-              ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Column(
               children: [
-                TableRow(
-                  children: [
-                    textBox('marketing expertise:', 'Tech expert'),
-                    textBox('experence', '8 years'),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    textBox('wored with', 'Jolt, Monday, Salesforce'),
-                    textBox('avg.increase in sales', '+146%'),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(28.0, 16.0, 28.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Stefan Garlson',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '4.8',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 12.0,
+                              ),
+                              Icon(Icons.star,
+                                  size: 20,
+                                  color: Colors.deepOrangeAccent[400]),
+                              Icon(Icons.star,
+                                  size: 20,
+                                  color: Colors.deepOrangeAccent[400]),
+                              Icon(Icons.star,
+                                  size: 20,
+                                  color: Colors.deepOrangeAccent[400]),
+                              Icon(Icons.star,
+                                  size: 20,
+                                  color: Colors.deepOrangeAccent[400]),
+                              Icon(Icons.star_half,
+                                  size: 20,
+                                  color: Colors.deepOrangeAccent[400]),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Transform.scale(
+                        scale: 0.8,
+                        child: Builder(
+                          builder: (ctx) {
+                            return FloatingActionButton(
+                              elevation: 0.0,
+                              child: Icon(Icons.add),
+                              onPressed: () {
+                                ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+                                  content: Text('hello'),
+                                ));
+                                print('clicked');
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      Transform.scale(
+                        scale: 0.8,
+                        child: FloatingActionButton(
+                            child: Icon(Icons.home),
+                            onPressed: () {
+                              Fluttertoast.showToast(
+                                msg: "This is Center Short Toast",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.blue,
+                                textColor: Colors.white,
+                                fontSize: 16.0,
+                              );
+                            }),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Table(
+                border: TableBorder.all(
+                  width: 1.0,
+                  style: BorderStyle.solid,
+                  color: Colors.grey.shade200,
+                ),
+                children: [
+                  TableRow(
+                    children: [
+                      textBox('marketing expertise:', 'Tech expert'),
+                      textBox('experence', '8 years'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      textBox('wored with', 'Jolt, Monday, Salesforce'),
+                      textBox('avg.increase in sales', '+146%'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
